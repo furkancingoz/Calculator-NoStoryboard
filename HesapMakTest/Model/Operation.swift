3f218a42f4f0f73004dca4a0cec4e075988fa1a1
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2024-02-06 23:58:32 +0300

    update

---
+++ HesapMakTest/Model/Operation.swift
@@ -0,0 +1,12 @@
+//
+//  Operation.swift
+//  HesapMakTest
+//
+//  Created by Furkan Cingöz on 6.02.2024.
+//
+
+import Foundation
+
+enum Operation {
+    case add, subtract, multiply, divide, none
+}
